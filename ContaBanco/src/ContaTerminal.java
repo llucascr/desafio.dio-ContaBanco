a9b09324f6060377fc7e788cc5e0f937b5c17d61
llucascr <lucasdecamposranzani@gmail.com>
2025-03-13 22:02:23 -0300

    feat: Tratamento de Exceção

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ContaTerminal {
@@ -5,23 +6,30 @@ public class ContaTerminal {
 
         Scanner scanner = new Scanner(System.in);
 
-        int numero;
+        int numero = 0;
         String agencia;
         String nomeCliente;
-        double saldo;
+        double saldo = 0;
 
         System.out.println("Por favor, digite o número da Agência !");
         agencia = scanner.next();
 
-        System.out.println("Digite o numero da Conta !");
-        numero = scanner.nextInt();
-        scanner.nextLine();
+        try {
+            System.out.println("Digite o numero da Conta !");
+            numero = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Digite o saldo do Cliente !");
+            saldo = scanner.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("Os campos numero da conta e saldo precisao ser numericos");
+            return;
+        } finally {
+            scanner.close();
+        }
 
         System.out.println("Digite o nome do Cliente !");
         nomeCliente = scanner.nextLine();
-
-        System.out.println("Digite o saldo do Cliente !");
-        saldo = scanner.nextDouble();
 
         System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, " +
                 "conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia, numero, saldo);
